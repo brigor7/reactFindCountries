9f3bb503c4fcc86546a74cdb5596cf3a05bc15ff
=Bruno Igor <=brigor7@gmail.com>
2020-06-17 17:18:42 -0300

    Ajustes no código

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Countries from './components/countries/Countries';
+import Header from './components/header/Header';
 
 export default class App extends Component {
   constructor() {
@@ -6,6 +8,9 @@ export default class App extends Component {
 
     this.state = {
       allCountries: [],
+      filteredCountries: [],
+      filteredPopulation: 0,
+      filter: '',
     };
   }
 
@@ -13,15 +18,65 @@ export default class App extends Component {
     const res = await fetch('https://restcountries.eu/rest/v2/all');
     const json = await res.json();
 
+    const allCountries = json.map(({ name, numericCode, flag, population }) => {
+      return {
+        id: numericCode,
+        name,
+        filteredName: name.toLowerCase(),
+        flag,
+        population,
+      };
+    });
+
+    const filteredPopulation = this.calculateTotalPopulationFrom(allCountries);
+
     this.setState({
-      allCountries: json,
+      allCountries,
+      filteredCountries: allCountries,
+      filteredPopulation,
     });
   }
-  render() {
+
+  calculateTotalPopulationFrom = (countries) => {
+    const totalPopulation = countries.reduce((acc, curr) => {
+      return acc + curr.population;
+    }, 0);
+    return totalPopulation;
+  };
+
+  handleChangeFilter = (newText) => {
+    this.setState({
+      filter: newText,
+    });
     const { allCountries } = this.state;
+    const filteredCountries = allCountries.filter((country) => {
+      return country.filteredName.includes(newText.toLowerCase());
+    });
+
+    const filteredPopulation = this.calculateTotalPopulationFrom(
+      filteredCountries
+    );
+
+    this.setState({
+      filteredCountries,
+      filteredPopulation,
+    });
+  };
+
+  countPopulation = (filteredCountries) => {};
+
+  render() {
+    const { filteredCountries, filteredPopulation, filter } = this.state;
     return (
       <div className="container">
         <h3>React Find Countries</h3>
+        <Header
+          filter={filter}
+          countryCount={filteredCountries.length}
+          totalPopulation={filteredPopulation}
+          onChangeFilter={this.handleChangeFilter}
+        />
+        <Countries countries={filteredCountries} />
       </div>
     );
   }
